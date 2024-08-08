7886336b8bed02bca97f5251a89d5b565d3e2b88
biancacramos <bianca.ramos777@gmail.com>
2024-08-08 16:15:59 -0300

    fix: remove unused code

--- lib/material_design.rb
+++ lib/material_design.rb
@@ -1,7 +1,6 @@
 require "material_design/version"
 require "material_design/engine"
-require "material_design/railtie" if defined?(Rails)
+require "material_design/railtie"
 
 module MaterialDesign
-  # Your code goes here...
 end
