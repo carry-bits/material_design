5bc024c864394832ba8b34c70e6f83fda5611d89
biancacramos <bianca.ramos777@gmail.com>
2024-08-12 09:32:25 -0300

    feat: update version

--- app/helpers/material_design/button_helper.rb
+++ app/helpers/material_design/button_helper.rb
@@ -1,20 +1,35 @@
 module MaterialDesign
   module ButtonHelper
-    def md_button_to(name, options = nil, html_options = {})
-      style = html_options[:style] || "filled"
-      variant = html_options[:variant] || "primary"
-      html_options[:class] = "button--container button__#{style}--#{variant}"
-      button_to(options, html_options.except(:style, :variant)) do
-        content_tag(:div, class: "layer-state layer-state__#{style}") do
+    def md_button_to(name = nil, options = nil, html_options = {})
+      variant = html_options[:variant] || "filled"
+      color = html_options[:color] || "primary"
+      icon = html_options[:icon]
+      html_options[:class] = "button--container button__#{variant}--#{color}"
+
+      button_to(options, html_options.except(:variant, :color, :icon)) do
+        content_tag(:div, class: "layer-state layer-state__#{variant}") do
           content_tag(:div, class: "button--content") do
-            name
+            concat(render("material_design/icons/icon", locals: { icon: icon, size: 18 })) if icon
+            concat(name)
           end
         end
       end
     end
 
-    def md_button_tag(label)
-      content_tag(:p, label, class: "test-hehe")
+    def md_button_tag(name = nil, content_or_options = nil, html_options = {})
+      variant = html_options[:variant] || "filled"
+      color = html_options[:color] || "primary"
+      icon = html_options[:icon]
+      html_options[:class] = "button--container button__#{variant}--#{color}"
+
+      button_tag(content_or_options, html_options.except(:variant, :color)) do
+        content_tag(:div, class: "layer-state layer-state__#{variant}") do
+          content_tag(:div, class: "button--content") do
+            concat(render("material_design/icons/icon", locals: { icon: icon, size: 18 })) if icon
+            concat(name)
+          end
+        end
+      end
     end
   end
 end
