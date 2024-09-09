9a4b2507430b772eb0d23fbb51634c6fce94ce33
biancacramos <bianca.ramos777@gmail.com>
2024-09-09 10:44:33 -0300

    fix: change svg for check circle 'filled'

--- lib/material_design/railtie.rb
+++ lib/material_design/railtie.rb
@@ -1,10 +1,10 @@
 require "rails/railtie"
 
 module MyGem
-  class Railtie < Rails::Railtie
+  class Railtie < Rails::Railtie  #add uma funcionalidade a um app rails a partir de uma gem
     initializer "material_design.action_controller" do
-      ActiveSupport.on_load(:action_controller_base) do
-        helper MaterialDesign::ButtonHelper
+      ActiveSupport.on_load(:action_controller_base) do #quando a base controller for carregada
+        helper MaterialDesign::ButtonHelper #disponibiliza o helper em todos os arquivos
       end
     end
   end
