a8e1d5c97cc9cef76de85d28cd1b691628417638
biancacramos <bianca.ramos777@gmail.com>
2024-10-15 15:53:53 -0300

    feat: create chip helper and start building styles

--- app/helpers/material_design/chip_helper.rb
+++ app/helpers/material_design/chip_helper.rb
@@ -1,8 +1,12 @@
 module MaterialDesign
   module ChipHelper
-    def md_filter_chip(label=nil, leading_icon: nil, trailing_icon: nil)
-      content_tag(:div, class: "chip") do
-        concat(render("material_design/icons/icon", locals: { icon: leading_icon, size: 18 })) if leading_icon
+    def md_filter_chip(label=nil, leading_icon: nil, trailing_icon: nil, selected: false, data: {})
+      content_tag(:div, class: class_names("chip", selected: selected), data: data) do
+        if selected
+          concat(render("material_design/icons/icon", locals: { icon: "check", size: 18 }))
+        else
+          concat(render("material_design/icons/icon", locals: { icon: leading_icon, size: 18 })) if leading_icon
+        end
         concat(content_tag(:p, label, class: "chip__label"))
         concat(render("material_design/icons/icon", locals: { icon: trailing_icon, size: 18 })) if trailing_icon
       end
