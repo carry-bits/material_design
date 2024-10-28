73b377455b37db37f60d4f327e57a3d4159d376e
sabinopa <priasabino@gmail.com>
2024-10-28 13:54:19 -0300

    fix: add arguments and change styles

--- app/helpers/material_design/menu_helper.rb
+++ app/helpers/material_design/menu_helper.rb
@@ -1,20 +1,31 @@
 module MaterialDesign
   module MenuHelper
-    def md_menu(menu_options: [], data: {})
-      content_tag(:div, class: "dropdown-menu hidden", data: data) do
+    def md_menu(menu_options: [], data: {}, form: nil, attribute: nil)
+      content_tag(:div, class: "dropdown-menu", data: data) do
         menu_options.each do |option|
-          concat(dropdown_menu_option(option))
+          concat(dropdown_menu_option(option, form, attribute))
         end
       end
     end
 
-    def dropdown_menu_option(option)
-      link_to option[:path], data: option[:data], method: option[:method] do
-        content_tag(:div, class: class_names("dropdown-item", divider: option[:divider])) do
-          concat(render("material_design/icons/icon", locals: { icon: option[:icon], size: 18 })) if option[:icon]
-          concat(content_tag(:span, option[:label]))
+    def dropdown_menu_option(option, form, attribute)
+      if option[:path]
+        link_to option[:path], data: option[:data], method: option[:method] do
+          content_tag(:div, class: class_names("dropdown-item", divider: option[:divider])) do
+            concat(render("material_design/icons/icon", locals: { icon: option[:icon], size: 18 })) if option[:icon]
+            concat(content_tag(:span, option[:label]))
+          end
+        end
+      elsif form
+        form.label "#{attribute}_#{option[:value]}", option[:label] do
+          concat(form.radio_button attribute, option[:value], class: "hidden", onchange: option[:onchange], checked: option[:checked])
+          concat(content_tag(:div, class: class_names("dropdown-item", divider: option[:divider])) do
+            concat(render("material_design/icons/icon", locals: { icon: option[:icon], size: 18 })) if option[:icon]
+            concat(content_tag(:span, option[:label]))
+          end)
         end
       end
     end
   end
 end
+
