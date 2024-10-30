2d01e853637f9c27be5e5bad723180079939ffa2
Priscila Sabino <142045460+sabinopa@users.noreply.github.com>
2024-10-30 14:01:03 -0300

    feat: menu component (#17)
    * feat: create segmented button e chips
    
    * fix: delete unnecessary class
    
    * fix: delete empty lines
    
    * feat: add colours
    
    * fix: add text color
    
    * feat: release version 0.8.0 and update authors
    
    * feat: create chip helper and start building styles
    
    * feat: create chip helper and start building styles
    
    * feat: add selected hover
    
    * config: add importmaps in gemspec and initialize it in engine.rb, pin stimulus in importmaps
    
    * feat: configure stimulus and importmaps to work on app where the gem is used
    
    * feat: create dropdown container for filter chip
    
    * feat: add logic to chip filter with dropdown
    
    * feat: implement menu structure
    
    * feat: adjust list without button and add divider
    
    * fix: delete unnecessary file
    
    * fix: add arguments and change styles
    
    * fix: adjust css
    
    * fix: restore files and change method name
    
    * merge com main
    
    * fix: undo unnecessaries modifications
    
    * fix: wrong writing
    
    * fix: adjustments
    
    * fix: undo modifications
    
    * fix: delete file
    
    * fix: undelete file
    
    * fix: undelete file
    
    ---------
    
    Co-authored-by: biancacramos <bianca.ramos777@gmail.com>

--- lib/material_design/railtie.rb
+++ lib/material_design/railtie.rb
@@ -1,11 +1,12 @@
 require "rails/railtie"
 
-module MyGem
+module MaterialDesign
   class Railtie < Rails::Railtie
     initializer "material_design.action_controller" do
-      ActiveSupport.on_load(:action_controller_base) do
+      ActiveSupport.on_load(:action_controller_base) do 
         helper MaterialDesign::ButtonHelper
         helper MaterialDesign::ChipHelper
+        helper MaterialDesign::MenuHelper
       end
     end
   end
